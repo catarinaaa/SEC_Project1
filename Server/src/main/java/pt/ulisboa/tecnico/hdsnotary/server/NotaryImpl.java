c28b363390cc207d9e6cfded30e3cfde6e90ef7b
Catarina Brás <catarinasaomiguel@tecnico.ulisboa.pt>
2019-04-05 18:49:45 +0100

    #11 Try to implement cartao do cidadao

--- Server/src/main/java/pt/ulisboa/tecnico/hdsnotary/server/NotaryImpl.java
+++ Server/src/main/java/pt/ulisboa/tecnico/hdsnotary/server/NotaryImpl.java
@@ -2,6 +2,7 @@ package pt.ulisboa.tecnico.hdsnotary.server;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -9,6 +10,7 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
@@ -25,36 +27,47 @@ import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.Signature;
 import java.security.SignatureException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.TreeMap;
 import pt.ulisboa.tecnico.hdsnotary.library.*;
-
+import pteidlib.PTEID_Certif;
+import pteidlib.PteidException;
+import pteidlib.pteid;
+import sun.security.pkcs11.wrapper.CK_ATTRIBUTE;
+import sun.security.pkcs11.wrapper.CK_C_INITIALIZE_ARGS;
+import sun.security.pkcs11.wrapper.CK_MECHANISM;
+import sun.security.pkcs11.wrapper.CK_SESSION_INFO;
+import sun.security.pkcs11.wrapper.PKCS11;
+import sun.security.pkcs11.wrapper.PKCS11Constants;
 
 public class NotaryImpl extends UnicastRemoteObject implements NotaryInterface, Serializable {
 
 	private static final String ALGORITHM = "SHA1withDSA";
 	private static final long serialVersionUID = 1L;
 	private final static String PATH = "storage/database.txt";
-	
+
 	// Singleton
 	private static NotaryImpl instance = null;
 
 	// To be changed **********************
 	private Random random = new Random();
-    // ************************************
-	
-    // List containing all goods
+	// ************************************
+
+	// List containing all goods
 	private TreeMap<String, Good> goodsList = new TreeMap<>();
-	
+
 	// List containing goods that are for sale
 	private ArrayList<String> goodsToSell = new ArrayList<String>();
 
 	// List containing nounces for security
 	private TreeMap<String, String> nounceList = new TreeMap<>();
-	
+
 	private File file = null;
 	private BufferedReader input = null;
 	private BufferedWriter output = null;
@@ -67,7 +80,7 @@ public class NotaryImpl extends UnicastRemoteObject implements NotaryInterface, 
 	protected NotaryImpl() throws RemoteException {
 		super();
 		populateList();
-		
+
 		try {
 			file = new File(PATH);
 			if (!file.exists()) {
@@ -95,13 +108,15 @@ public class NotaryImpl extends UnicastRemoteObject implements NotaryInterface, 
 			output = new BufferedWriter(new FileWriter(file, true));
 			recoverTransactions();
 			printGoods();
+			
+			setupCititzenCard();
 
-		} catch (IOException | NoSuchAlgorithmException | InvalidKeyException e) {
+		} catch (IOException | NoSuchAlgorithmException | InvalidKeyException | CertificateException | PteidException e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
 	}
-	
+
 	public static NotaryImpl getInstance() {
 		if (instance == null) {
 			try {
@@ -121,7 +136,7 @@ public class NotaryImpl extends UnicastRemoteObject implements NotaryInterface, 
 		nounceList.put(userId, nounce.toString());
 		return nounce.toString();
 	}
-	
+
 	@Override
 	public Result intentionToSell(String userId, String goodId, String cnounce, byte[] signature)
 			throws RemoteException {
@@ -183,16 +198,16 @@ public class NotaryImpl extends UnicastRemoteObject implements NotaryInterface, 
 				goodsToSell.remove(goodId);
 				saveTransfer(sellerId, buyerId, goodId);
 				printGoods();
-				System.out.println(msg+"true");
+				System.out.println(msg + "true");
 				return new Result(true, cnounce, signMessage(msg + "true"));
 			}
 		}
 		printGoods();
 		return new Result(false, cnounce, signMessage(msg + "false"));
 	}
-	
-	//...
-	
+
+	// ...
+
 	private KeyPair generateKeys() throws NoSuchAlgorithmException {
 		// Gerar par de chave publica e privada
 		KeyPairGenerator keygen = KeyPairGenerator.getInstance("DSA");
@@ -253,7 +268,7 @@ public class NotaryImpl extends UnicastRemoteObject implements NotaryInterface, 
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	private static String bytesToHex(byte[] hash) {
@@ -342,5 +357,62 @@ public class NotaryImpl extends UnicastRemoteObject implements NotaryInterface, 
 		}
 		return null;
 	}
+
+	private void setupCititzenCard() throws PteidException, CertificateException {
+		System.out.println("            //Load the PTEidlibj");
+		System.out.println(System.getProperty("java.library.path"));
+		System.loadLibrary("pteidlibj");
+		pteid.Init(""); // Initializes the eID Lib
+		pteid.SetSODChecking(false); // Don't check the integrity of the ID, address and photo (!)
+
+		PKCS11 pkcs11;
+		String osName = System.getProperty("os.name");
+		String javaVersion = System.getProperty("java.version");
+		System.out.println("Java version: " + javaVersion);
+
+		java.util.Base64.Encoder encoder = java.util.Base64.getEncoder();
+
+		String libName = "libpteidpkcs11.so";
+
+		// access the ID and Address data via the pteidlib
+		System.out.println("            -- accessing the ID  data via the pteidlib interface");
+
+		X509Certificate cert = getCertFromByteArray(getCertificateInBytes(0));
+		System.out.println("Citized Authentication Certificate " + cert);
+
+	}
+	
+	//Returns the CITIZEN AUTHENTICATION CERTIFICATE
+    public static byte[] getCitizenAuthCertInBytes(){
+        return getCertificateInBytes(0); //certificado 0 no Cartao do Cidadao eh o de autenticacao
+    }
+
+    // Returns the n-th certificate, starting from 0
+    private static  byte[] getCertificateInBytes(int n) {
+        byte[] certificate_bytes = null;
+        try {
+            PTEID_Certif[] certs = pteid.GetCertificates();
+            System.out.println("Number of certs found: " + certs.length);
+            int i = 0;
+	    for (PTEID_Certif cert : certs) {
+                System.out.println("-------------------------------\nCertificate #"+(i++));
+                System.out.println(cert.certifLabel);
+            }
+
+            certificate_bytes = certs[n].certif; //gets the byte[] with the n-th certif
+
+            //pteid.Exit(pteid.PTEID_EXIT_LEAVE_CARD); // OBRIGATORIO Termina a eID Lib
+        } catch (PteidException e) {
+            e.printStackTrace();
+        }
+        return certificate_bytes;
+    }
+
+    public static X509Certificate getCertFromByteArray(byte[] certificateEncoded) throws CertificateException{
+        CertificateFactory f = CertificateFactory.getInstance("X.509");
+        InputStream in = new ByteArrayInputStream(certificateEncoded);
+        X509Certificate cert = (X509Certificate)f.generateCertificate(in);
+        return cert;
+    }
 
 }
